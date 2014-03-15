a7dc2a065dda13f47c6b2fcf9a7b583ebda10f52
Casuldo <kasper.apajalahti@gmail.com>
2014-03-15 14:40:19 +0200

    j

---
+++ server.js
@@ -0,0 +1,71 @@
+var static = require('node-static');
+var http = require('http');
+var file = new(static.Server)('C:\\Users\\Kassu\\WebRTC\\room');
+var app = http.createServer(function (req, res) {
+  file.serve(req, res);
+}).listen(1234);
+
+
+// var express = require('express');
+// var app = express();
+// console.log(express.static(__dirname + '/js'));
+// app.use(express.static(__dirname + '/js'));
+// app.all('*', function(req, res){
+// 	res.sendfile("index.html");
+// });
+
+// app.listen(9000);
+
+
+var io = require('socket.io').listen(app);
+io.sockets.on('connection', function (socket){
+
+	function log(){
+		var array = [">>> "];
+	  for (var i = 0; i < arguments.length; i++) {
+	  	array.push(arguments[i]);
+	  }
+	    socket.emit('log', array);
+	}
+
+	socket.on('message', function (message) {
+		log('Got message: ', message);
+		socket.broadcast.emit('message', message); // should be room only
+	});
+
+	socket.on('create', function (room) {
+		var numClients = io.sockets.clients(room).length;
+
+		log('Room ' + room + ' has ' + numClients + ' client(s)');
+		log('Create a room', room);
+		socket.emit('created', room);
+		socket.emit('emit(): client ' + socket.id + ' joined room ' + room);
+		socket.broadcast.emit('broadcast(): client ' + socket.id + ' joined room ' + room);
+
+	});
+	
+	socket.on('join', function (room) {
+		var numClients = io.sockets.clients(room).length;
+
+		log('Room ' + room + ' has ' + numClients + ' client(s)');
+		log('Request to join a room', room);
+
+		if (numClients == 0){
+			socket.join(room);
+			socket.emit('created', room);
+		} else if (numClients < 10) {
+			io.sockets.in(room).emit('join', room);
+			socket.join(room);
+			socket.emit('joined', room);
+		} else { // max two clients
+			socket.emit('full', room);
+		}
+		socket.emit('emit(): client ' + socket.id + ' joined room ' + room);
+		socket.broadcast.emit('broadcast(): client ' + socket.id + ' joined room ' + room);
+
+	});
+
+});
+
+
+console.log("CHECK");
